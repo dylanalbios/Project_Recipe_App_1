91efff9b58d1d65aaad33f64720993db316acbe4
dylanalbios <dylxanj430@gmail.com>
2023-05-09 12:39:07 -0700

    basic structure of forms and text areas

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -6,12 +6,89 @@ function RecipeCreate() {
   // TODO: Add the required input and textarea form elements.
   // TODO: Add the required submit and change handlers
   
+  const [name, setName] = useState("");
+  const handleNameChange = (event) => setName(event.target.value);
+
+  const [cuisine, setCuisine] = useState("");
+  const handleCuisineChange = (event) => setCuisine(event.target.value);
+
+  const [photo, setPhoto] = useState("");
+  const handlePhotoChange = (event) => setPhoto(event.target.value);
+
+  const [ingredients, setIngredients] = useState("");
+  const handleIngredientsChange = (event) => setIngredients(event.target.value);
+
+  const [preparation, setPreparation] = useState("");
+  const handlePreparationChange = (event) => setreparation(event.target.value);
+
+
+
+
   return (
     <form name="create">
       <table>
         <tbody>
           <tr>
-            <td></td>
+            <td>
+              <label htmlFor="name">
+                <input
+                  id="name" 
+                  type="text" 
+                  name="name" 
+                  onChange={handleNameChange} 
+                  value={name} 
+                  placeholder="Name"
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="cuisine">
+                <input
+                  id="cuisine" 
+                  type="text" 
+                  name="cuisine" 
+                  onChange={handleCuisineChange} 
+                  value={cuisine} 
+                  placeholder="Cuisine"
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="photo">
+                <input
+                  id="photo" 
+                  type="text" 
+                  name="photo" 
+                  onChange={handlePhotoChange} 
+                  value={photo} 
+                  placeholder="URL"
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="ingredients">
+                <textarea
+                  id="ingredients" 
+                  type="text" 
+                  name="ingredients" 
+                  onChange={handleIngredientsChange} 
+                  value={ingredients} 
+                  placeholder="Ingredients"
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="preparation">
+                <textarea
+                  id="preparation" 
+                  type="text" 
+                  name="preparation" 
+                  onChange={handlePreparationChange} 
+                  value={preparation} 
+                  placeholder="Preparation"
+                />
+              </label>
+            </td>
             <td>
               <button type="submit">Create</button>
             </td>
