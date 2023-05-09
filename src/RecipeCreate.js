05757be478905b2f89148a057c0dec687b06f4e1
dylanalbios <dylxanj430@gmail.com>
2023-05-09 13:43:33 -0700

    organized and added create button functionality

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,31 +1,40 @@
 import React, { useState } from "react";
 
-function RecipeCreate() {
+function RecipeCreate({ onRecipeCreate }) {
 
   // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
   // TODO: Add the required input and textarea form elements.
   // TODO: Add the required submit and change handlers
   
+
   const [name, setName] = useState("");
-  const handleNameChange = (event) => setName(event.target.value);
-
   const [cuisine, setCuisine] = useState("");
-  const handleCuisineChange = (event) => setCuisine(event.target.value);
-
   const [photo, setPhoto] = useState("");
-  const handlePhotoChange = (event) => setPhoto(event.target.value);
-
   const [ingredients, setIngredients] = useState("");
-  const handleIngredientsChange = (event) => setIngredients(event.target.value);
-
   const [preparation, setPreparation] = useState("");
+  
+
+  const handleNameChange = (event) => setName(event.target.value);
+  const handleCuisineChange = (event) => setCuisine(event.target.value);
+  const handlePhotoChange = (event) => setPhoto(event.target.value);
+  const handleIngredientsChange = (event) => setIngredients(event.target.value);
   const handlePreparationChange = (event) => setPreparation(event.target.value);
 
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const recipe = { name, cuisine, photo, ingredients, preparation };
+    onRecipeCreate(recipe);
+    setName("");
+    setCuisine("");
+    setPhoto("");
+    setIngredients("");
+    setPreparation("");
+  };
 
 
   return (
-    <form name="create">
+    <form name="create" onSubmit={handleSubmit}>
       <table>
         <tbody>
           <tr>
