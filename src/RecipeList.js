094f6173e80fc4dbcef7dd7a394e325b02d68a7f
dylanalbios <dylxanj430@gmail.com>
2023-05-09 13:39:46 -0700

    added delete buttona and each component for the recipe

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-function RecipeList() {
+function RecipeList({ recipes, onRecipeDelete }) {
   
   // TODO: Display the list of recipes using the structure of table that is provided.
   // TODO: Create at least one additional component that is used by this component.
   // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
+
+  const handleDelete= (id) => {
+    onRecipeDelete(id);
+  };
+
+
+
+
 
   return (
     <div className="recipe-list">
@@ -20,7 +28,18 @@ function RecipeList() {
           </tr>
         </thead>
         <tbody>
-        
+          {recipes.map((recipe) => (
+            <tr key={recipes.id}>
+              <td>{recipe.name}</td>
+              <td>{recipe.cuisine}</td>
+              <td><img src={recipe.photo} alt={recipe.name} /></td>
+              <td>{recipe.ingredients}</td>
+              <td>{recipe.preparation}</td>
+              <td>
+                <button onClick={() => handleDelete(recipe.id)} name="delete">Delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
